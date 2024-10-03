cdaa8164cc484d11dc9535d4bd91952d1417309a
sandcastle3 <sandcastle3gh@gmail.com>
2024-10-03 16:11:22 -0400

    qol fixes

--- static/index.js
+++ static/index.js
@@ -1,39 +1,52 @@
 const extra_score = document.getElementById("score");
 const extra_multiplier = document.getElementById("multiplier");
+const max_multiplier = document.getElementById("max_multiplier");
 const current_score = document.getElementById("current_score");
 const current_multiplier = document.getElementById("current_multiplier");
+
 let score = parseInt(getCookieValue("score")) || 0;
 let multiplier = parseInt(getCookieValue("multiplier")) || 1;
+let multiplier_cost = 100 * multiplier;
 
-function getCookieValue(name) {
+function getCookieValue(name){
     const cookieArr = document.cookie.split(';');
-    for (let i = 0; i < cookieArr.length; i++) {
-        const cookiePair = cookieArr[i].trim();
-        if (cookiePair.startsWith(name + '=')) {
+    for (let cookie of cookieArr){
+        const cookiePair = cookie.trim();
+        if (cookiePair.startsWith(name + '=')){
             return cookiePair.split('=')[1];
         }
     }
     return null;
 }
 
-function update() {
+function update(){
+    multiplier_cost = 100 * multiplier;
     document.cookie = `score=${score}; expires=Wed, 01 Jan 2030 00:00:00 UTC`;
     document.cookie = `multiplier=${multiplier}; expires=Wed, 01 Jan 2030 00:00:00 UTC`;
-    current_score.innerHTML = `score: ${getCookieValue("score")}`;
-    current_multiplier.innerHTML = `multiplier: x${getCookieValue("multiplier")}`;
+    current_score.innerHTML = `score: ${score}`;
+    current_multiplier.innerHTML = `multiplier: x${multiplier}`;
+    extra_multiplier.innerHTML = `x2 multiplier (${multiplier_cost} score)`
 }
 
 extra_score.onclick = function(){
     score += multiplier;
     update();
-}
+};
 
 extra_multiplier.onclick = function(){
-    if (score >= 100){
-        score -= 100;
-        multiplier = multiplier * 2;
+    if (score >= multiplier_cost) {
+        score -= multiplier_cost;
+        multiplier *= 2;
         update();
     }
-}
+};
+
+max_multiplier.onclick = function(){
+    while (score >= multiplier_cost) {
+        score -= multiplier_cost;
+        multiplier *= 2;
+        update();
+    }
+};
 
 update();
