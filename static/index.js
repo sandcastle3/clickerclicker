404db0d566219b0b70fa7220858885cf18930e44
sandcastle3 <sandcastle3gh@gmail.com>
2024-10-03 19:00:19 -0400

    add sps (score per second)

--- static/index.js
+++ static/index.js
@@ -1,13 +1,18 @@
 const extra_score = document.getElementById("score");
 const extra_multiplier = document.getElementById("multiplier");
+const extra_sps = document.getElementById("sps");
 const max_multiplier = document.getElementById("max_multiplier");
+const max_sps = document.getElementById("max_sps");
 const current_score = document.getElementById("current_score");
 const current_multiplier = document.getElementById("current_multiplier");
+const current_sps = document.getElementById("current_sps");
 const full_reset = document.getElementById("full_reset");
 
 let score = parseInt(getCookieValue("score")) || 0;
 let multiplier = parseInt(getCookieValue("multiplier")) || 1;
+let sps = parseInt(getCookieValue("sps")) || 0;
 let multiplier_cost = 100 * multiplier;
+let sps_cost = 50 * sps;
 
 function getCookieValue(name){
     const cookieArr = document.cookie.split(';');
@@ -20,13 +25,22 @@ function getCookieValue(name){
     return null;
 }
 
+function dosps(){
+    score += sps;
+    update();
+}
+
 function update(){
     multiplier_cost = 100 * multiplier;
+    sps_cost = 50 * sps;
     document.cookie = `score=${score}; expires=Wed, 01 Jan 2030 00:00:00 UTC`;
     document.cookie = `multiplier=${multiplier}; expires=Wed, 01 Jan 2030 00:00:00 UTC`;
+    document.cookie = `sps=${sps}; expires=Wed, 01 Jan 2030 00:00:00 UTC`
     current_score.innerHTML = `score: ${score}`;
     current_multiplier.innerHTML = `multiplier: x${multiplier}`;
+    current_sps.innerHTML = `sps: ${sps}`
     extra_multiplier.innerHTML = `x2 multiplier (${multiplier_cost} score)`
+    extra_sps.innerHTML = `+1 sps (${sps_cost} score)`
 }
 
 extra_score.onclick = function(){
@@ -42,6 +56,14 @@ extra_multiplier.onclick = function(){
     }
 };
 
+extra_sps.onclick = function(){
+    if (score >= sps_cost) {
+        score -= sps_cost;
+        sps += 1;
+        update();
+    }
+};
+
 max_multiplier.onclick = function(){
     while (score >= multiplier_cost) {
         score -= multiplier_cost;
@@ -50,10 +72,20 @@ max_multiplier.onclick = function(){
     }
 };
 
+max_sps.onclick = function(){
+    while (score >= sps_cost) {
+        score -= sps_cost;
+        sps += 1;
+        update();
+    }
+}
+
 full_reset.onclick = function(){
     score = 0;
     multiplier = 1;
+    sps = 0;
     update();
 };
 
+setInterval(dosps, 1000);
 update();
